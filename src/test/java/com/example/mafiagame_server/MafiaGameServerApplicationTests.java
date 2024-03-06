6f8a5601e094e4e1689fa9da83285aa7ac042e48
NoteSeonjin <hawooks112@gmail.com>
2024-03-06 14:25:02 +0900

    # 초기 생성 커밋

---
+++ src/test/java/com/example/mafiagame_server/MafiaGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mafiagame_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaGameServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
